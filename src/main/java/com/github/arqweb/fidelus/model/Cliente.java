337e5c8a5171844fc0a5fb5ec6dabfe792c73715
Warxwell <senhorpompas@gmail.com>
2021-10-05 19:47:43 -0300

    Se agregan las clases de cliente y regla

---
+++ src/main/java/com/github/arqweb/fidelus/model/Cliente.java
@@ -0,0 +1,117 @@
+package com.github.arqweb.fidelus.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+    @Id
+    @Column(name = "id")
+    @Basic(optional = false)
+    @GeneratedValue(generator = "clienteSec", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "clienteSec", sequenceName = "cliente_sec", allocationSize = 0)
+    private int id;
+
+    @Column(name = "nombre", length = 50)
+    @Basic(optional = false)
+    private String nombre;
+
+    @Column(name = "apellido", length = 50)
+    @Basic(optional = false)
+    private String apellido;
+
+    @Column(name = "nro_documento", length = 50)
+    @Basic(optional = false)
+    private String nroDocumento;
+
+    @Column(name = "tipo_documento", length = 50)
+    @Basic(optional = false)
+    private String tipoDocumento;
+
+    @Column(name = "nacionalidad", length = 50)
+    @Basic(optional = false)
+    private String nacionalidad;
+
+    @Column(name = "email", length = 50)
+    private String email;
+
+    @Column(name = "telefono", length = 50)
+    private String telefono;
+
+    @Column(name = "fecha_nacimiento")
+    @Basic(optional = false)
+    private Date fechaNacimiento;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNroDocumento() {
+        return nroDocumento;
+    }
+
+    public void setNroDocumento(String nroDocumento) {
+        this.nroDocumento = nroDocumento;
+    }
+
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+}
---
+++ src/main/java/com/github/arqweb/fidelus/model/Regla.java
@@ -0,0 +1,72 @@
+package com.github.arqweb.fidelus.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "regla")
+public class Regla {
+
+    @Id
+    @Column(name = "id")
+    @Basic(optional = false)
+    @GeneratedValue(generator = "reglaSec", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "reglaSec", sequenceName = "regla_sec", allocationSize = 0)
+    private int id;
+
+    @Column(name = "limite_max")
+    @Basic(optional = false)
+    private Integer limiteMax;
+
+    @Column(name = "limite_min")
+    @Basic(optional = false)
+    private Integer limiteMin;
+
+    @Column(name = "monto_equivalencia")
+    @Basic(optional = false)
+    private Integer montoEquivalencia;
+
+    @Column(name = "id_vencimiento")
+    @Basic(optional = false)
+    private Integer idVencimiento;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Integer getLimiteMax() {
+        return limiteMax;
+    }
+
+    public void setLimiteMax(Integer limiteMax) {
+        this.limiteMax = limiteMax;
+    }
+
+    public Integer getLimiteMin() {
+        return limiteMin;
+    }
+
+    public void setLimiteMin(Integer limiteMin) {
+        this.limiteMin = limiteMin;
+    }
+
+    public int getMontoEquivalencia() {
+        return montoEquivalencia;
+    }
+
+    public void setMontoEquivalencia(Integer montoEquivalencia) {
+        this.montoEquivalencia = montoEquivalencia;
+    }
+
+    public Integer getIdVencimiento() {
+        return idVencimiento;
+    }
+
+    public void setIdVencimiento(Integer idVencimiento) {
+        this.idVencimiento = idVencimiento;
+    }
+
+}
