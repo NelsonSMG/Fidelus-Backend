d257c7e673e46f97d152ca9f24f313d8e42991fc
Eduardo Maciel <tachi.maciel98@gmail.com>
2021-10-16 11:42:41 -0300

    Concepto-Parametrizacion

---
+++ src/main/java/com/github/arqweb/fidelus/model/VencimientoPuntos.java
@@ -0,0 +1,57 @@
+package com.github.arqweb.fidelus.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "vencimiento_puntos")
+public class VencimientoPuntos {
+
+    @Id
+    @Column(name = "id")
+    @Basic(optional = false)
+    @GeneratedValue(generator = "vencimientoPuntosSec", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "vencimientoPuntosSec", sequenceName = "vencimiento_puntos_sec", allocationSize = 0)
+    private int id;
+
+    @Column(name = "fecha_ini_validez")
+    private Date fechaIniValidez;
+
+    @Column(name = "fecha_fin_validez")
+    private Date fechaFinValidez;
+
+    @Column(name = "duracion")
+    private int duracion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getFechaIniValidez() {
+        return fechaIniValidez;
+    }
+
+    public void setFechaIniValidez(Date fechaIniValidez) {
+        this.fechaIniValidez = fechaIniValidez;
+    }
+
+    public Date getFechaFinValidez() {
+        return fechaFinValidez;
+    }
+
+    public void setFechaFinValidez(Date fechaFinValidez) {
+        this.fechaFinValidez = fechaFinValidez;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+}
---
+++ src/main/java/com/github/arqweb/fidelus/rest/ConceptoCanjeREST.java
@@ -0,0 +1,48 @@
+package com.github.arqweb.fidelus.rest;
+import com.github.arqweb.fidelus.ejb.ConceptoCanjeDAO;
+import com.github.arqweb.fidelus.model.ConceptoCanje;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+@Path("concepto-canje")
+@Consumes("application/json")
+@Produces("application/json")
+public class ConceptoCanjeREST {
+
+    @Inject
+    private ConceptoCanjeDAO conceptoCanjeDao;
+
+    @GET
+    @Path("/")
+    public Response listar() {
+        return Response.ok(conceptoCanjeDao.obtenerClientesPorParametro()).build();
+    }
+
+    @POST
+    @Path("/")
+    public Response crear(ConceptoCanje conceptoCanje) {
+        this.conceptoCanjeDao.agregar(conceptoCanje);
+        return Response.ok().build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response eliminar(@PathParam(value = "id") Integer id){
+        try {
+            this.conceptoCanjeDao.eliminar(id);
+            return Response.ok().build();
+        }catch (Exception a) {
+            return Response.serverError().build();
+        }
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response actualizar(@PathParam(value = "id") Integer id, ConceptoCanje conceptoCanje) {
+        conceptoCanje.setId(id);
+        this.conceptoCanjeDao.actualizar(conceptoCanje);
+        return Response.ok().build();
+    }
+}
