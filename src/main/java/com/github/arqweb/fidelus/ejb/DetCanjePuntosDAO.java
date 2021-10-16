438d07b705f942f983c0bb30c3ff23cb073201f6
Eduardo Maciel <tachi.maciel98@gmail.com>
2021-10-16 17:28:43 -0300

    Finalizacion del detalle y actualizacion del archivo Persistence

---
+++ src/main/java/com/github/arqweb/fidelus/ejb/DetCanjePuntosDAO.java
@@ -0,0 +1,31 @@
+package com.github.arqweb.fidelus.ejb;
+
+
+import com.github.arqweb.fidelus.model.DetCanjePuntos;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
+
+@Stateless
+public class DetCanjePuntosDAO {
+
+    @PersistenceContext(unitName = "fidelizacionPU")
+    private EntityManager em;
+
+
+    public void guardarDetalles(Integer idCanje, List<DetCanjePuntos> detalles){
+        for (DetCanjePuntos det : detalles) {
+            det.setIdCanje(idCanje);
+            this.em.persist(det);
+        }
+    }
+    public List<DetCanjePuntos> obtenerDetallesPorCanje(Integer idCanje){
+        Query q = this.em.createQuery("select d from DetCanjePuntos d where d.idCanje = :param");
+        q.setParameter("param", idCanje);
+
+        return (List<DetCanjePuntos>) q.getResultList();
+    }
+}
