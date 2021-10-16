10e16d3838c724d93a09897064a8ec12a1a66a96
Warxwell <senhorpompas@gmail.com>
2021-10-16 13:33:47 -0300

    Implementación de clases DAO y REST para bolsa de puntos

--- src/main/java/com/github/arqweb/fidelus/ejb/BolsaPuntosDAO.java
+++ src/main/java/com/github/arqweb/fidelus/ejb/BolsaPuntosDAO.java
@@ -1,2 +1,68 @@
-package com.github.arqweb.fidelus.ejb;public class BolsaPuntosDAO {
+package com.github.arqweb.fidelus.ejb;
+
+import com.github.arqweb.fidelus.model.BolsaPuntos;
+
+import java.util.Date;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.ArrayList;
+import java.util.List;
+
+@Stateless
+public class BolsaPuntosDAO {
+
+    @PersistenceContext(unitName = "fidelizacionPU")
+    private EntityManager em;
+    @Inject
+    private ReglaDAO reglaDAO;
+    @Inject
+    private VencimientoPuntosDAO vencimientoPuntosDAO;
+
+    public List<BolsaPuntos> listar(){
+        Query q = this.em.createQuery("select p from BolsaPuntos p");
+        List<BolsaPuntos> bolsaPuntos = (List<BolsaPuntos>) q.getResultList();
+        return bolsaPuntos;
+    }
+
+    public void agregar(BolsaPuntos entidad) {
+        this.em.persist(entidad);
+    }
+
+    public BolsaPuntos obtenerPorId(Integer id){
+        return this.em.find(BolsaPuntos.class, id);
+    }
+
+    public void eliminar(Integer id) {
+        try {
+            BolsaPuntos entity = obtenerPorId(id);
+            this.em.remove(entity);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public void actualizar(BolsaPuntos entidad) {
+        this.em.merge(entidad);
+    }
+
+    public void calcBolsaPuntos(Integer idCliente, Integer monto) throws Exception {
+        try {
+            Integer puntos = reglaDAO.obtenerEquivalenciaPuntos(monto);
+            BolsaPuntos bolsa = new BolsaPuntos();
+            bolsa.setIdCliente(idCliente);
+            bolsa.setMonto(monto);
+            bolsa.setPuntajeAsignado(puntos);
+            bolsa.setSaldo(puntos);
+            bolsa.setFechaAsignacion(new Date());
+            bolsa.setFechaVencimiento(vencimientoPuntosDAO.calculaFechaVencimiento(new Date()));
+            agregar(bolsa);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
 }
--- src/main/java/com/github/arqweb/fidelus/rest/BolsaPuntosRest.java
+++ src/main/java/com/github/arqweb/fidelus/rest/BolsaPuntosRest.java
@@ -1,2 +1,31 @@
-package com.github.arqweb.fidelus.rest;public class BolsaPuntosRest {
+package com.github.arqweb.fidelus.rest;
+
+import com.github.arqweb.fidelus.ejb.BolsaPuntosDAO;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+@Path("bolsa-puntos")
+@Consumes("application/json")
+@Produces("application/json")
+public class BolsaPuntosRest {
+
+    @Inject
+    private BolsaPuntosDAO bolsaPuntosDAO;
+
+    @GET
+    @Path("/")
+    public Response listar() {
+        return Response.ok(bolsaPuntosDAO.listar()).build();
+    }
+
+    @POST
+    @Path("/")
+    public Response agregarPuntos(@QueryParam("idCliente") Integer idCliente,
+                                  @QueryParam("monto") Integer monto) throws Exception {
+        this.bolsaPuntosDAO.calcBolsaPuntos(idCliente, monto);
+        return Response.ok().build();
+    }
+
 }
