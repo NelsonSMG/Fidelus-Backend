90d14b80890e2b5bca7c223551802b0b60ee81bd
Warxwell <senhorpompas@gmail.com>
2021-10-26 20:43:53 -0300

    Tarea planificada - Función que corre cada 24 horas para actualizar bolsa de puntos

--- src/main/java/com/github/arqweb/fidelus/ejb/TareaPlanificadaDAO.java
+++ src/main/java/com/github/arqweb/fidelus/ejb/TareaPlanificadaDAO.java
@@ -2,6 +2,10 @@ package com.github.arqweb.fidelus.ejb;
 
 import com.github.arqweb.fidelus.model.BolsaPuntos;
 
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.inject.Inject;
@@ -12,9 +16,18 @@ public class TareaPlanificadaDAO {
     @Inject
     private BolsaPuntosDAO bolsaPuntosDAO;
 
-    @Schedule(minute = "*/1", hour = "*", persistent = false)
-    public void atSchedule() throws InterruptedException {
-        System.out.println("Probando tarea planificada");
+    @Schedule(minute = "*", hour = "*/24", persistent = false)
+    public void actBolsaPuntos() throws InterruptedException {
+        List<BolsaPuntos> bolsaPuntos = bolsaPuntosDAO.listar();
+        System.out.print("Actualizacion planificada de bolsa de puntos");
+        if(bolsaPuntos != null && !bolsaPuntos.isEmpty()) {
+            List<BolsaPuntos> bolsasVencidas = bolsaPuntos.stream().filter(
+                    bolsa -> bolsa.getFechaVencimiento().before(new Date())).collect(Collectors.toList());
+            for(BolsaPuntos bolsa : bolsasVencidas) {
+                bolsa.setSaldo(0);
+                bolsaPuntosDAO.actualizar(bolsa);
+            }
+        }
     }
 
 }
